c1b799326f67069f0684c7f2548725a3c6ca7afc
evilBream <evilBream@yandex.com>
2024-11-17 23:34:31 +0500

    ui components n routing (no logic)

---
+++ deepthought/app/chat/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+import Chat from "../ui/chat"
+
+export default function Page(){
+    return (
+        <div className="main-content">
+          <p>Chat</p>
+          <Chat />
+        </div>
+    );
+}
